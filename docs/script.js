a9457d6af3b5dde7b98c64296ae47ece83ab02b1
kimura <hirofumi_kimura@seki-shin.net>
2026-01-14 22:46:21 +0900

    12th commit

--- docs/script.js
+++ docs/script.js
@@ -62,7 +62,9 @@ joinBtn.onclick = async () => {
         statusLabel.innerText = "認証中...";
         
         // 💡 サーバーに名前も送るように修正
-        const response = await fetch(`https://isdw7jpzcj.execute-api.ap-northeast-3.amazonaws.com/default/study_group_main/token?password=${password}&username=${encodeURIComponent(username)}`);
+        // 💡 URLの中間の "study_group_main" を削除した形にします
+        const baseUrl = "https://isdw7jpzcj.execute-api.ap-northeast-3.amazonaws.com/default";
+        const response = await fetch(`${baseUrl}/token?password=${password}&username=${encodeURIComponent(username)}`);
 
         if (response.status === 401) throw new Error("合言葉が違います");
         if (!response.ok) throw new Error("サーバーとの通信に失敗しました");
